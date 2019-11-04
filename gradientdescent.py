25de8a0edafe0d60c40c21eefe047c1af24e50dd
Parisa Khan <parisak916@gmail.com>
2019-11-04 17:24:21 -0500

    Update gradientdescent.py

--- gradientdescent.py
+++ gradientdescent.py
@@ -12,7 +12,7 @@ def gd_hw3(x1,x2,alpha,iter):
     x1 = 0
     x2 = 0 
     plt.figure()
-
+#original equation:  f(x) = 3x1^2 + 2x2^2 + 4x1x2 – 5x1 + 6.
     for j in range(len(alpha)):
         for i in range(iterations):
             x1 = x1 - alpha[j]*(6*x1 + 4*x2)
@@ -22,10 +22,7 @@ def gd_hw3(x1,x2,alpha,iter):
         plt.show()
         
 
-alpha = arr.array('d',[.0001, .001, .01, .1, 1, 10])
-
-xfinal = gd_hw3(0,0,alpha, iter=1000)
-
+#original equation: f(x) = sin(x) + 0.3x
 def gd_h3_2(x, alpha, iter):
     plt.figure()
     plt.ylim((-10,10))
@@ -39,7 +36,11 @@ def gd_h3_2(x, alpha, iter):
         plt.show()
         
 
-
+#step function
 alpha = arr.array('d',[.0001, .001, .01, .1, 1, 10])
 x=2
+# x starts at 2, different values of x converge at different local minimums
 xfinal = gd_h3_2(x,alpha, iter=1000)
+#gradient of x1 and x2 start at (0,0)
+xfinal2 = gd_hw3(0,0,alpha, iter=1000)
+
